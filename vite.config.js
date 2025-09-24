f14f632c8f1a6c7dc0f2ff3e8f46c9c72ad9ee6d
shanmukha919 <potturishanmukha919@gmail.com>
2025-09-24 08:36:38 +0530

    v1.1.1

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,16 @@ export default defineConfig({
   plugins: [react()],
   base: '/CICD_HOSPITAL_MANEGEMENT_DEPLOY', // or '/' depending on your routing and folder structure
   server: {
+    host: true,
+    port: 3000,
     proxy: {
-      '/api': 'http://localhost:8091'
+      '/api': {
+        target: 'http://localhost:8091',
+        changeOrigin: true
+      }
     }
+  },
+  build: {
+    outDir: 'dist'
   }
 })
-
-
